b94eda6e4c0e57f5f63602f6193f8942b25bb9a7
imparnets <163425461+imparnets@users.noreply.github.com>
2024-03-14 10:59:47 -0300

    upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function calcularDiaria() {
+    var diaria = 0;
+
+    if (document.getElementById('semestral').value !== '') {
+        diaria = (parseFloat(document.getElementById('semestral').value) * 0.9) / 12;
+    } else if (document.getElementById('anual').value !== '') {
+        diaria = (parseFloat(document.getElementById('anual').value) / 2 * 0.9) / 12;
+    } else if (document.getElementById('trimestral').value !== '') {
+        diaria = (parseFloat(document.getElementById('trimestral').value) * 2 * 0.9) / 12;
+    } else if (document.getElementById('mensal').value !== '') {
+        diaria = (parseFloat(document.getElementById('mensal').value) * 6 * 0.9) / 12;
+    }
+
+    document.getElementById('resultado').innerHTML = 'A diária é: ' + diaria.toFixed(2);
+}
